37886079def22ab3732e1c942e905b9c18348a38
Daniil <dpancov64@gmail.com>
2022-07-31 20:27:33 +0300

    better code

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:torch_controller/torch_controller.dart';
-import 'package:flutter/services.dart';
+import 'package:torch_light/torch_light.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  TorchController().initialize();
-  runApp(const MorningLight());
+void main() {
+  runApp(const TorchApp());
 }
 
-class MorningLight extends StatefulWidget {
-  const MorningLight({Key? key}) : super(key: key);
+class TorchApp extends StatefulWidget {
+  const TorchApp({Key? key}) : super(key: key);
 
   @override
-  _MorningLightState createState() => _MorningLightState();
+  _TorchAppState createState() => _TorchAppState();
 }
 
-class _MorningLightState extends State<MorningLight> {
-  final controller = TorchController();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+class _TorchAppState extends State<TorchApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TorchController(),
+    );
+  }
+}
 
-  TimeOfDay time = const TimeOfDay(hour: 10, minute: 30);
+class TorchController extends StatelessWidget {
+  const TorchController({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TimeOfDay time = const TimeOfDay(hour: 10, minute: 30);
     final hours = time.hour.toString().padLeft(2, '0');
     final minutes = time.minute.toString().padLeft(2, '0');
-
+    
     return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      body: FutureBuilder<bool>(
+        future: _isTorchAvailable(context),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.hasData && snapshot.data!) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      child: const Text('Enable torch'),
+                      onPressed: () async {
+                        _enableTorch(context);
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      child: const Text('Disable torch'),
+                      onPressed: () {
+                        _disableTorch(context);
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
-            '${hours}:${minutes}',
+            '$hours:$minutes',
             style: const TextStyle(fontSize: 32),
           ),
           const SizedBox(height: 16),
@@ -41,11 +71,57 @@ class _MorningLightState extends State<MorningLight> {
 
                 if (newTime == null) return;
 
-                setState(() => time = newTime);
+                time = newTime;
               },
               child: const Text('Select Time'))
-        ]),
+        ])
+                ),
+              ],
+            );
+          } else if (snapshot.hasData) {
+            return const Center(
+              child: Text('No torch available.'),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
+  }
+
+  Future<bool> _isTorchAvailable(BuildContext context) async {
+    try {
+      return await TorchLight.isTorchAvailable();
+    } on Exception catch (_) {
+      _showMessage(
+        'Could not check if the device has an available torch',
+        context,
+      );
+      rethrow;
+    }
+  }
+
+  Future<void> _enableTorch(BuildContext context) async {
+    try {
+      await TorchLight.enableTorch();
+    } on Exception catch (_) {
+      _showMessage('Could not enable torch', context);
+    }
+  }
+
+  Future<void> _disableTorch(BuildContext context) async {
+    try {
+      await TorchLight.disableTorch();
+    } on Exception catch (_) {
+      _showMessage('Could not disable torch', context);
+    }
+  }
+
+  void _showMessage(String message, BuildContext context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 }
